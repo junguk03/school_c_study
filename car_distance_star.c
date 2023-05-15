a16ab3beb30ec16e88b89ed172ba37845f60e338
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-05-15 16:11:08 +0900

    Add files via upload

---
+++ car_distance_star.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void disp_car(int car_number,int distance){
+    int i;
+    printf("car #%d",car_number);
+    for(i=0;i<distance/10;i++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+int main(void){
+    int i;
+    int car1_dist = 0, car2_dist = 0;
+    srand((unsigned)time(NULL));
+    for(i=0;i<6;i++){
+        car1_dist += rand()%100;
+        car2_dist += rand()%100;
+        disp_car(1,car1_dist);
+        disp_car(2,car2_dist);
+        printf("-------------------\n");
+    }
+    return 0;
+}
